154a9c37dd37ae57dfd109980c485347705db860
rabbit <rabbit@6b7f18f4-c3aa-419c-a9ba-557306c87171>
2009-10-03 23:15:20 +0000

    Fix boss screenshots.

--- trunk/snapthatshit.lua
+++ trunk/snapthatshit.lua
@@ -20,6 +20,13 @@ function addon:ADDON_LOADED(mod)
 	local pName = UnitName("player")
 	PaparazziGnomeDB[pName] = PaparazziGnomeDB[pName] or {}
 	db = PaparazziGnomeDB[pName]
+	for k in pairs(db) do
+		if type(k) == "string" then
+			local numericId = tonumber(k:sub(-12, -7), 16)
+			db[numericId] = true
+			db[k] = nil
+		end
+	end
 end
 
 do
@@ -53,11 +60,12 @@ function addon:PLAYER_LEVEL_UP() ss = true end
 function addon:ACHIEVEMENT_EARNED() ss = true end
 
 function addon:COMBAT_LOG_EVENT_UNFILTERED(time, event, sGuid, sName, sFlags, dGuid, dName, dFlags)
-	if event ~= "UNIT_DIED" or not bosses[dGuid] then return end
+	if event ~= "UNIT_DIED" or UnitIsDeadOrGhost("player") or not bosses[dGuid] then return end
+	local numericId = tonumber(dGuid:sub(-12, -7), 16)
 	-- Because of this, we no longer screenshot on new boss kills if the player is dead,
 	-- but I can't be arsed to add a "is raid in combat" scan right now.
-	if db[dGuid] or UnitIsDeadOrGhost("player") then return end
-	db[dGuid] = true
+	if db[numericId] then return end
+	db[numericId] = true
 	if InCombatLockdown() then
 		ssWhenOOC = true
 	else
